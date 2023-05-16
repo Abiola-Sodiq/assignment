1fed6c2820e1da2848c7f8ae0a962d0dba88fdd5
abiola-sodiq <rasababe01@gmail.com>
2023-05-16 13:52:44 +0100

    new commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const body = document.body;
 
 const div = document.createElement("div");
 const div2 = document.createElement("div")
+const text = document.createElement("h1")
 const span1 = document.createElement("span")
 const span2 = document.createElement("span")
 const span3 = document.createElement("span")
@@ -18,10 +19,11 @@ div.classList.add("maincontainer")
 div2.classList.add("container")
 body.append(div)
 div.append(div2)
-div2.append(span1,span2,span3)
+div2.append(text,span1,span2,span3)
 span1.append(img,img2,img3)
 span2.append(img4,img5,img6)
 span3.append(img7,img8,img9)
+text.innerText = "Sodiq Abiola Rukayat Picture Grid"
 img.setAttribute("src","./images/abibi1.JPG" )
 img2.setAttribute("src","./images/abibi2.JPG")
 img3.setAttribute("src","./images/abibi3.JPG")
@@ -31,9 +33,6 @@ img6.setAttribute("src","./images/abibi6.JPG")
 img7.setAttribute("src","./images/abibi7.JPG")
 img8.setAttribute("src","./images/abibi8.JPG")
 img9.setAttribute("src","./images/abibi9.JPG")
-
-
-
 
 
 
