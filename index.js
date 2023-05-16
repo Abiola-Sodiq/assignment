a3d17475c49de9ffbfb5f5464fe655a545921a1e
abiola-sodiq <rasababe01@gmail.com>
2023-05-16 11:35:57 +0100

    new commit

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const body = document.body;
 
 const div = document.createElement("div");
+const div2 = document.createElement("div")
+const span1 = document.createElement("span")
+const span2 = document.createElement("span")
+const span3 = document.createElement("span")
 const img = document.createElement("img");
-
-img.setAttribute("src", "https://wallpapers.com/images/hd/funny-dog-wallpaper-t0etwwk8zmw7nbl8.jpg" )
-console.log(img);
-div.classList.add("container")
+const img2 = document.createElement("img")
+const img3 = document.createElement("img")
+const img4 = document.createElement("img")
+const img5 = document.createElement("img")
+const img6 = document.createElement("img")
+const img7 = document.createElement("img")
+const img8 = document.createElement("img")
+const img9 = document.createElement("img")
+div.classList.add("maincontainer")
+div2.classList.add("container")
 body.append(div)
-div.append(img)
+div.append(div2)
+div2.append(span1,span2,span3)
+span1.append(img,img2,img3)
+span2.append(img4,img5,img6)
+span3.append(img7,img8,img9)
+img.setAttribute("src","./images/abibi1.JPG" )
+img2.setAttribute("src","./images/abibi2.JPG")
+img3.setAttribute("src","./images/abibi3.JPG")
+img4.setAttribute("src","./images/abibi4.JPG")
+img5.setAttribute("src","images/abibi5.jpg")
+img6.setAttribute("src","./images/abibi6.JPG")
+img7.setAttribute("src","./images/abibi7.JPG")
+img8.setAttribute("src","./images/abibi8.JPG")
+img9.setAttribute("src","./images/abibi9.JPG")
+
+
+
+
+
+
+
+
+
+
